51f1b423e62f13d08146a512883f75ddf13f1a00
Surya Pratap <85509906+singham2000@users.noreply.github.com>
2022-09-25 22:08:16 +0530

    Answer

--- que1.java
+++ que1.java
@@ -1,4 +1,4 @@
-class SwapNumbers {
+public class que1 {
     public static void main(String[] args) {
         int i = 10, j = 20;
         System.out.println("First number = " + i);
--- que2.java
+++ que2.java
@@ -1,4 +1,4 @@
-class printNaturalNumbers {
+public class que2 {
     public static void main(String[] args) {
         int number = 0;
         for (int i = 1; i <= 50; i++) {
